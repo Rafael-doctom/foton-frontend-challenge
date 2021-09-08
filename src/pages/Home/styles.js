257b9735aa56e75cb449a2a32c8018cf86cb9653
Joao Pedro <jpresende502@gmail.com>
2021-09-07 21:25:10 -0300

    feat: adding animations in home page

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,12 +1,40 @@
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 import { Link } from "react-router-dom"
+
+const LoadingAnimationTopToBottom = keyframes`
+    from {
+        opacity: 0;
+        transform: translateY(16px);
+    }
+    to {
+        opacity: 1;
+    }
+`
+const LoadingAnimationBottomToTop = keyframes`
+    from {
+        opacity: 0;
+        transform: translateY(-16px);
+    }
+    to {
+        opacity: 1;
+    }
+`
+const LoadingAnimationLeftToRight = keyframes`
+    from {
+        opacity: 0;
+        transform: translateX(-16px);
+    }
+    to {
+        opacity: 1;
+    }
+`
 
 const Header = styled.header`
     display: flex;
     flex-direction: column;
     background-image: linear-gradient(#ffe3c7a1 75%, #fff);
     text-align: center;
-
+    margin-bottom: 13px;
     @media(min-width: 700px){
         display: flex;
         flex-direction: row-reverse;
@@ -22,6 +50,8 @@ const SearchInputWrapper = styled.div`
 `
 
 const SearchInput = styled.input`
+    transition: 0.4s;
+    animation: ${LoadingAnimationTopToBottom} 0.6s linear 1;
     text-indent: 42px;
     font-size: 1.3rem;
     margin-bottom: 13px;
@@ -30,7 +60,6 @@ const SearchInput = styled.input`
     border: none;
     width: 80%;
     outline: none;
-
     &:focus::placeholder {
         color: #000;
         transition: 0.4s;
@@ -39,11 +68,13 @@ const SearchInput = styled.input`
 
 const SearchIcon = styled.i`
     position: absolute;
+    animation: ${LoadingAnimationBottomToTop} 0.7s linear 1;
     padding: 20px;
     min-width: 40px;
 `
 
 const UserWelcomeText = styled.h1`
+    animation: ${LoadingAnimationLeftToRight} 0.8s linear 1;
     color: #54565A;
     width: 100%;
     padding: 8px 0;
@@ -61,10 +92,24 @@ const Books = styled.section`
     display: flex;
     flex-wrap: wrap;
     flex-direction: row;
+    @media(min-width:  1000px){
+        img {
+            width: 30%;
+        }
+        div {
+            width: 30%;
+        }
+    }
 `
 
 const Book = styled.div`
+    animation: ${LoadingAnimationTopToBottom} 1s linear 1;
     width: 40%;
+    transition: 0.4s;
+
+    &:hover{
+        color: #7154c1;
+    }
 `
 
 const BookTitle = styled.h3`
